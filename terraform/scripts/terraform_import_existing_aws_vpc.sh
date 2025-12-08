dab0cc749b32869266bfb7068663d49d9b45228b
Levin <136597282+levin-fankhauser@users.noreply.github.com>
2025-12-08 11:20:24 +0100

    fix: Rename all occurrences of myapp to sketchly (#92) (#95)

--- terraform/scripts/terraform_import_existing_aws_vpc.sh
+++ terraform/scripts/terraform_import_existing_aws_vpc.sh
@@ -2,7 +2,7 @@
 
 # INFO: $1   : Beinhaltet der erste Parameter, der dem Script übergeben wird
 #       2>&1 : Sendet Errors nach /dev/null, damit das Script nicht beendet wird
-result=$(aws ec2 describe-vpcs --filter Name=tag:App,Values=myapp 2>&1)
+result=$(aws ec2 describe-vpcs --filter Name=tag:App,Values=sketchly 2>&1)
 
 if [ $? -eq 0 ]; then
   echo "$result" | jq -c ".Vpcs" | while read -r vpc; do
