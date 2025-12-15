08071ed6af8f49577a2512415213718287e7b089
Levin <levin.fankhauser@gmx.ch>
2025-12-15 10:42:16 +0100

    feat: Implement changes from PR (#9)

--- app/app/draw/[id]/page.tsx
+++ app/app/draw/[id]/page.tsx
@@ -6,6 +6,10 @@ import "tldraw/tldraw.css";
 export default function App() {
   const { id } = useParams();
 
+  if (!id || typeof id !== "string") {
+    return <div>Invalid canvas ID</div>;
+  }
+
   return (
     <div className="min-h-screen md:pl-10">
       <div className="h-screen" data-testid="tldraw-editor">
